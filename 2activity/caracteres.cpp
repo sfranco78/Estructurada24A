cf67698cab246d8afd88a110da29aefeaf43e592
Sergio Franco <scasillas@cualtos.udg.mx>
2024-02-02 10:55:13 -0600

    segunda actividad

---
+++ 2activity/caracteres.cpp
@@ -0,0 +1,27 @@
+/* *******
+*** NOMBRE: SERGIO FRANCO CASILLAS
+*** FECHA: 2/2/2024
+*** DESCRIPCION: UTILIZACION DE CARACTERES ESPECIALES
+*** VERSION 1.0
+*** CUALTOS - UDG
+*****/
+
+#include <iostream>
+using namespace std;
+
+// inicio de MAIN
+int main(){
+   int edad;
+
+   edad = 18;
+   cout << "Mi nombre es Sergio Franco " << endl;
+   cout << "\n" << "Mi edad es: " << edad << endl;
+
+return 0;
+} // fin de main
+
+
+
+
+
+
