f278bc98aa7646051e84dcfc017186e476325724
Sergio Franco <scasillas@cualtos.udg.mx>
2024-02-27 12:56:48 -0600

    libs

---
+++ Modular/Principal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include "mylib.cpp"
+
+
+
+
+int main(){
+
+    hello();
+    imp("Mi nombre es:");
+    impln("Sergio Franco Casillas");
+    imp(50);
+    imp(12.5);
+
+
+    return 1;
+}//
+
+
+
---
+++ Modular/mylib.cpp
@@ -0,0 +1,42 @@
+
+// Definición de funciones
+/*
+  Funcion de saludo inicial
+*/
+void hello(){
+    cout << "Welcome to my first lib program" << endl;
+    cout << "      Version 1.0" << endl;
+    cout << "*******************************" << endl;
+} //fin de hello
+
+void imp(string texto){
+    cout << texto;
+}//fin de imp
+
+void impln(string texto){
+    cout << texto << endl;
+}//fin de imp
+
+void imp(int num){
+    cout << num << endl;
+}//fin de imp
+
+void imp(double num){
+    cout << num << endl;
+}//fin de imp
+
+bool comprueba(char val){
+    if(val == 'a')
+        return true;
+    else
+        return false;
+}// fin comprueba
+
+
+
+
+
+
+
+
+
